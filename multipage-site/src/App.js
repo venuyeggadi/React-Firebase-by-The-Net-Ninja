8ed8f0eb2423131d0aaada7122cc75936ae93dca
Venu Yeggadi <yeggadivenu@gmail.com>
2022-10-14 14:50:13 +0530

    Redirect user to home page in case of wrong route and Add query parameters for contact page

--- multipage-site/src/App.js
+++ multipage-site/src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, NavLink, Redirect } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -30,6 +30,9 @@ function App() {
           </Route>
           <Route path="/articles/:id">
             <Article />
+          </Route>
+          <Route path="*">
+            <Redirect to="/"/>
           </Route>
         </Switch>
       </BrowserRouter>
--- multipage-site/src/pages/Contact.js
+++ multipage-site/src/pages/Contact.js
@@ -1,7 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 const Contact = () => {
+  const queryString = useLocation().search;
+
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get("name");
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name} Contact us here...</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio temporibus perferendis accusamus autem porro amet ea ipsa, nihil vero dolore minima culpa fugit, accusantium in laboriosam repellat dolorum voluptate expedita.</p>
     </div>
   );
