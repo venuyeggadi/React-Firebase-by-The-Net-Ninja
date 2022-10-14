657db3a6854996c492b09b39f47a0872de33aa2e
Venu Yeggadi <yeggadivenu@gmail.com>
2022-10-14 11:49:27 +0530

    Add a route for article page

---
+++ multipage-site/src/pages/Article.js
@@ -0,0 +1,9 @@
+const Article = () => {
+  return (
+    <div>
+      Article page
+    </div>
+  );
+}
+ 
+export default Article;
--- multipage-site/src/pages/Home.js
+++ multipage-site/src/pages/Home.js
@@ -1,4 +1,6 @@
 import { useFetch } from "../hooks/useFetch";
+import { Link } from "react-router-dom";
+
 
 import './Home.css';
 
@@ -14,6 +16,7 @@ const Home = () => {
         <div key={article.id} className="card">
           <h3>{article.title}</h3>
           <p>{article.author}</p>
+          <Link to={`/articles/${article.id}`}>Read more...</Link>
         </div>
       ))}
     </div>
