e5cc55d40650fae1570cdcd6b05c5497bba153ae
Venu Yeggadi <yeggadivenu@gmail.com>
2022-10-14 12:53:41 +0530

    Fetch and show article based on id and redirect to homepage if article doesn't exist

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,9 +1,34 @@
+import { useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
 const Article = () => {
+  const { id } = useParams();
+  const url = `http://localhost:3000/articles/${id}`;
+  const { data: article, isPending, error } = useFetch(url);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push('/')
+      }, 2000);
+    }
+  }, [error, history])
+
   return (
     <div>
-      Article page
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {article && (
+        <div>
+          <h3>{article.title}</h3>
+          <p>By {article.author}</p>
+          <p>{article.body}</p>
+        </div>
+      )}
     </div>
   );
 }
- 
+
 export default Article;
