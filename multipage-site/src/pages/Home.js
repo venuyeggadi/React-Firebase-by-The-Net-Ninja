d5f26a8ebdd46cb75ce17ab0a0c6b4adddbb47c2
Venu Yeggadi <yeggadivenu@gmail.com>
2022-10-14 11:38:32 +0530

    Fetch and show articles in homepage

--- multipage-site/src/pages/Home.js
+++ multipage-site/src/pages/Home.js
@@ -1,8 +1,21 @@
+import { useFetch } from "../hooks/useFetch";
+
+import './Home.css';
+
 const Home = () => {
+  const { data: articles, isPending, error } = useFetch('http://localhost:3000/articles');
+ 
   return ( 
-    <div>
-      <h2>Homepage</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio temporibus perferendis accusamus autem porro amet ea ipsa, nihil vero dolore minima culpa fugit, accusantium in laboriosam repellat dolorum voluptate expedita.</p>
+    <div className="home">
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles && articles.map((article) => (
+        <div key={article.id} className="card">
+          <h3>{article.title}</h3>
+          <p>{article.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
