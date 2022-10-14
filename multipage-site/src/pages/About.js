3b616f039587a82a3e0f540d5772ec719e73168f
Venu Yeggadi <yeggadivenu@gmail.com>
2022-10-14 09:55:21 +0530

    Setup React Router

---
+++ multipage-site/src/pages/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio temporibus perferendis accusamus autem porro amet ea ipsa, nihil vero dolore minima culpa fugit, accusantium in laboriosam repellat dolorum voluptate expedita.</p>
+    </div>
+  );
+}
+ 
+export default About;
---
+++ multipage-site/src/pages/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio temporibus perferendis accusamus autem porro amet ea ipsa, nihil vero dolore minima culpa fugit, accusantium in laboriosam repellat dolorum voluptate expedita.</p>
+    </div>
+  );
+}
+ 
+export default Contact;
